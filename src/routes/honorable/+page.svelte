42ac17ecfe8dcf02ce615fb8247bb5c45e90bd8e
Modestly Jen <jen@jenniferluchka.com>
2023-08-16 14:06:28 -0400

    fix bugs

--- src/routes/honorable/+page.svelte
+++ src/routes/honorable/+page.svelte
@@ -6,12 +6,6 @@
 <div class="text-column">
 	<h1>About this app</h1>
 
-	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
-	</p>
-
-	<pre>npm create svelte@latest</pre>
 
 	<p>
 		The page you're looking at is purely static HTML, with no client-side interactivity needed.
@@ -19,8 +13,5 @@
 		the devtools network panel and reloading.
 	</p>
 
-	<p>
-		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
-		using it with JavaScript disabled!
-	</p>
+	
 </div>
